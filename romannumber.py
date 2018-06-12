3664c043775ad03846ddcb39d8c5657080f7f8dd
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-12 11:49:48 +0530

    Create romannumber.py

---
+++ romannumber.py
@@ -0,0 +1,43 @@
+string=input()
+if string=="I":
+	print(1)
+elif string=="II":
+	print(2)
+elif string=="III":
+	print(3)
+elif string=="IV":
+	print(4)
+elif string=="V":
+	print(5)
+elif string=="VI":
+	print(6)
+elif string=="VII":
+	print(7)
+elif string=="VIII":
+	print(8)
+elif string=="IX":
+	print(9)
+elif string=="X":
+	print(10)
+elif string=="XI":
+	print(11)
+elif string=="XII":
+	print(12)
+elif string=="XIII":
+	print(13)
+elif string=="XIV":
+	print(14)
+elif string=="XV":
+	print(15)
+elif string=="XVI":
+	print(16)
+elif string=="XVII":
+	print(1)
+elif string=="XVIII":
+	print(1)
+elif string=="XIX":
+	print(1)
+elif string=="XX":
+	print(1)
+else:
+	print("value exceeded 20")
