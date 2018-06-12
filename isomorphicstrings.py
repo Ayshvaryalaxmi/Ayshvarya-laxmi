2d2c63c9412bf8f99fe700451c2d8a81a0141952
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-12 12:29:22 +0530

    Create isomorphicstrings.py

---
+++ isomorphicstrings.py
@@ -0,0 +1,12 @@
+string=input()
+List=string.split(" ")
+n=len(List[0])
+m=len(List[1])
+if n==m:
+	for i in range(0,n):
+		if List[0][i]!=List[1][i]:
+			List[0]=List[0].replace(List[0][i],List[1][i])
+	if List[0]==List[1]:
+		print("yes")
+	else:
+		print("no")
