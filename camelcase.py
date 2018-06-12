e14d80be497f56238adde2fdac0b9a7d186b8385
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-12 12:01:40 +0530

    Update camelcase.py

--- camelcase.py
+++ camelcase.py
@@ -1,1 +1,9 @@
-print(input().title())
+#print(input().title())
+n=input()
+Lis=[]
+List=n.split(" ")
+for word in List:
+	word=word[0].upper()+word[1:].lower()
+	Lis.append(word)
+string=" ".join(Lis)
+print(string)
