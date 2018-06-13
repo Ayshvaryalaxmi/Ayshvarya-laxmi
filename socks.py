01a770c3700bcd59f6b3c4f1f45eb7f1b6b648b1
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-13 11:35:25 +0530

    Update socks.py

--- socks.py
+++ socks.py
@@ -1,46 +1,45 @@
 l=[]
-R=int(input())-1
-B=int(input())-1
-G=int(input())-1
+S=input()
+s=S.split(" ")
+R=int(s[0])
+B=int(s[1])
+G=int(s[2])
 N=int(input())
+total=R+B+G
 pairs=0
-def R_decerement():
-	if R>0:
-		R-=1
-		l.append("R")
-def B_decrement():
-	if B>0:
-		B-=1
-		l.append("B")
-def G_decrement():
-	if G>0:
-		G-=1
-		l.append("G")
-def main():
-	if N==1:
-		print("4")
-	else:
-		R-=1
-		B-=1
-		G-=1
-		l.append("R")
-		l.append("B")
-		l.append("G")
-		while(len(l))!=(R+B+G+3)):
-			for i in range(2):
-				R_decrement()
+if N==1:
+	print("4")
+else:
+	R-=1
+	B-=1
+	G-=1
+	l.append("R")
+	l.append("B")
+	l.append("G")
+	while len(l)<total:
+		for i in range(2):
+			if R>0 and pairs!=N:
+				R-=1
+				l.append("R")
 				if(R%2==0):
 					pairs+=1
-			for i in range(2):
-				B_decrement()
+		if pairs==(N):
+			break
+		for i in range(2):
+			if B>0 and pairs!=N:
+				B-=1
+				l.append("B")
 				if(B%2==0):
 					pairs+=1
-			for i in range(2):
-				G_decrement()
+		if pairs==(N):
+			break
+		for i in range(2):
+			if G>0 and pairs!=N:
+				G-=1
+				l.append("G")
 				if(G%2==0):
 					pairs+=1
-			if pairs==(N-3):
-				break
-		draws=len(l)
-		print(draws)
-main()
+		if pairs==(N):
+			break
+	draws=len(l)
+	print(draws)
