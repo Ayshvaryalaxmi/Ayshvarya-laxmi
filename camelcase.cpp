a7823c6f6be71f135de8eaed230eaa514dccd136
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-22 10:11:44 +0530

    Create camelcase.cpp

---
+++ camelcase.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	char str[100],word[20];
+	int i,j=0;
+	cout<<"enter the string\n";
+	cin.get(str,100);
+	for(i=0;str[i]!=]'\0';i++)
+	{if(str[i]!=' ')
+	 {word[j]=str[i];
+	  j++;
+	 }
+	 else
+	 {word[j]='\0';
+	  word[0]=toupper(word[0]);
+	  cout<<word<<' ';
+	  j=0;
+	 }
+	}
+	return 0;
+}
