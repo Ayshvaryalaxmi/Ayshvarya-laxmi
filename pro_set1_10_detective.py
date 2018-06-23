af4086f63d9e03a77f125ae2dbdc9ca3ffeea9da
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-06-23 12:28:49 +0530

    Update pro_set1_10_detective.py

--- pro_set1_10_detective.py
+++ pro_set1_10_detective.py
@@ -2,7 +2,11 @@ N=int(input())
 s=input()
 l=s.split(" ")
 sum=0
-for i in l:
-	for j in range(int(i)):
-		sum+=j
+for i in range(N):
+	for j in range(int(l[i])):
+		#print(l[:i])
+		#print(j)
+		m=str(j)
+		if m in l[:i]:
+			sum=sum+j
 print(sum)
