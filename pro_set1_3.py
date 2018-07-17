8a4178729659230717b267c4f17f2381b3d68e13
Ayshvaryalaxmi <34570252+Ayshvaryalaxmi@users.noreply.github.com>
2018-07-17 20:02:44 +0530

    Update pro_set1_3.py

--- pro_set1_3.py
+++ pro_set1_3.py
@@ -1,6 +1,9 @@
 s=input().split(" ")
-changes=len(s[1])-len(s[0])
-for num in range(len(s[0])):
+changes=abs(len(s[1])-len(s[0]))
+n=len(s[0])
+if n>len(s[1]):
+	n=len(s[1])
+for num in range(n):
 	if s[0][num]!=s[1][num]:
 		changes+=1
 print(changes)
